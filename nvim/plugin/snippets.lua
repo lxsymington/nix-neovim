7be3d645b8f2386ea367c6f7b2872ee65de1a30f
Luke Xavier Symington <lukexaviersymington@gmail.com>
2024-06-05 13:11:56 +0100

    Update

--- nvim/plugin/snippets.lua
+++ nvim/plugin/snippets.lua
@@ -61,11 +61,11 @@ ls.config.setup({
 	},
 })
 
-vim.keymap.set({ 'i', 's' }, '<c-;>', function()
+vim.keymap.set({ 'i', 's' }, '<c-k>', function()
 	return vim.snippet.active({ direction = 1 }) and vim.snippet.jump(1)
 end, { silent = true })
 
-vim.keymap.set({ 'i', 's' }, '<c-,>', function()
+vim.keymap.set({ 'i', 's' }, '<c-j>', function()
 	return vim.snippet.active({ direction = -1 }) and vim.snippet.jump(-1)
 end, { silent = true })
 
