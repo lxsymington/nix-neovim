810953d3403585a54c468fb189d98f5abdde6b8e
Luke Xavier Symington <lukexaviersymington@gmail.com>
2024-10-25 08:43:13 +0100

    ⁅Ticket: #N/A⁆ · Update Inputs

--- nvim/plugin/testing.lua
+++ nvim/plugin/testing.lua
@@ -16,10 +16,10 @@ neotest.setup({
 			jestCommand = 'npm run --silent test:integration --if-present -- ',
 			jest_test_discovery = true,
 		}),
-		require('lxs.testing.adapters.mocha')({
-			-- mochaCommand = 'npm run --silent test:local --if-present -- ',
-			mocha_test_discovery = true,
-		}),
+		-- require('lxs.testing.adapters.mocha')({
+		-- 	mochaCommand = 'npm run --silent test:local --if-present -- ',
+		-- 	mocha_test_discovery = true,
+		-- }),
 		require('neotest-vim-test')({
 			ignore_filetypes = { 'typescript', 'typescriptreact', 'javascript', 'javascriptreact' },
 		}),
