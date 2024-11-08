08a2384cefb2cfed49a7231554f1b51f806c6499
Luke Xavier Symington <lukexaviersymington@gmail.com>
2024-11-08 12:40:03 +0000

    ⁅Ticket: #N/A⁆ · Nixd
    Check for `nixd` executable instead of replaced `nil` executable.

--- nvim/ftplugin/nix.lua
+++ nvim/ftplugin/nix.lua
@@ -3,7 +3,7 @@ local lspconfig = require('lspconfig')
 local icons = require('mini.icons')
 
 -- Exit if the language server isn't available
-if vim.fn.executable('nil') ~= 1 then
+if vim.fn.executable('nixd') ~= 1 then
 	return
 end
 
