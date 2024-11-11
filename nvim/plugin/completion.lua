6b1d6fbc8fd8e7e91db6e101830b50c219320640
Luke Xavier Symington <lukexaviersymington@gmail.com>
2024-11-11 17:47:56 +0000

    ⁅Ticket: #N/A⁆ · Completion format

--- nvim/plugin/completion.lua
+++ nvim/plugin/completion.lua
@@ -4,6 +4,7 @@ end
 vim.g.did_load_completion_plugin = true
 
 local blink = require('blink.cmp')
+local utils = require('blink.cmp.utils')
 local copilot_comparators = require('copilot_cmp.comparators')
 local lspkind = require('lspkind')
 
@@ -19,7 +20,9 @@ lspkind.init({
 
 blink.setup({
 	accept = {
-		enabled = true,
+		auto_brackets = {
+			enabled = true,
+		},
 	},
 	fuzzy = {
 		prebuilt_binaries = {
@@ -90,10 +93,118 @@ blink.setup({
 		},
 	},
 	windows = {
+		autocomplete = {
+			draw = {
+				columns = {
+					{ 'kind_icon', gap = 1 },
+					{ 'label', 'label_description', gap = 1 },
+					{ 'kind', gap = 1 },
+				},
+				components = {
+					kind_icon = {
+						ellipsis = false,
+						text = function(ctx)
+							local padding = ctx.self.padding or 1
+							local left_padding, right_padding =
+								type(padding) == 'table' and unpack(ctx.self.padding) or padding, padding
+							return string.format(
+								'%s%s%s%s',
+								string.rep(' ', left_padding),
+								ctx.kind_icon,
+								string.rep(' ', right_padding),
+								ctx.icon_gap
+							)
+						end,
+						highlight = function(ctx)
+							return utils.get_tailwind_hl(ctx) or 'BlinkCmpKind' .. ctx.kind
+						end,
+					},
+
+					kind = {
+						ellipsis = false,
+						width = { fill = true },
+						text = function(ctx)
+							local padding = ctx.self.padding or 1
+							local left_padding, right_padding =
+								type(padding) == 'table' and unpack(ctx.self.padding) or padding, padding
+							return string.format(
+								'%s%s%s',
+								string.rep(' ', left_padding),
+								ctx.kind,
+								string.rep(' ', right_padding)
+							)
+						end,
+						highlight = function(ctx)
+							return utils.get_tailwind_hl(ctx) or 'BlinkCmpKind' .. ctx.kind
+						end,
+					},
+
+					label = {
+						width = { fill = true, max = 60 },
+						text = function(ctx)
+							local padding = ctx.self.padding or 1
+							local left_padding, right_padding =
+								type(padding) == 'table' and unpack(ctx.self.padding) or padding, padding
+							return string.format(
+								'%s%s%s',
+								string.rep(' ', left_padding),
+								ctx.label,
+								' ',
+								ctx.label_detail,
+								string.rep(' ', right_padding)
+							)
+						end,
+						highlight = function(ctx)
+							-- label and label details
+							local highlights = {
+								{
+									0,
+									#ctx.label,
+									group = ctx.deprecated and 'BlinkCmpLabelDeprecated' or 'BlinkCmpLabel',
+								},
+							}
+							if ctx.label_detail then
+								table.insert(
+									highlights,
+									{ #ctx.label, #ctx.label + #ctx.label_detail, group = 'BlinkCmpLabelDetail' }
+								)
+							end
+
+							-- characters matched on the label by the fuzzy matcher
+							for _, idx in ipairs(ctx.label_matched_indices) do
+								table.insert(highlights, { idx, idx + 1, group = 'BlinkCmpLabelMatch' })
+							end
+
+							return highlights
+						end,
+					},
+
+					label_description = {
+						width = { max = 30 },
+						ellipsis = true,
+						text = function(ctx)
+							local padding = ctx.self.padding or 1
+							local left_padding, right_padding =
+								type(padding) == 'table' and unpack(ctx.self.padding) or padding, padding
+							return string.format(
+								'%s%s%s',
+								string.rep(' ', left_padding),
+								ctx.label_description,
+								string.rep(' ', right_padding)
+							)
+						end,
+					},
+				},
+				gap = 0,
+				padding = 1,
+			},
+		},
 		documentation = {
 			auto_show = true,
+			border = 'none',
 		},
 		signature_help = {
+			border = 'none',
 			scrollbar = true,
 		},
 	},
